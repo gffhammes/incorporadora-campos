616c84ac7fe75defd94bbc9a4c87ec76b73f94a9
Guilherme Hammes <gffhammes@gmail.com>
2022-05-23 22:02:52 -0300

    fix: fix building type error in page title

--- src/components/commons/PageTitle.tsx
+++ src/components/commons/PageTitle.tsx
@@ -7,17 +7,15 @@ import { pages } from '../../constants/pages'
 export const PageTitle = () => {
   const { asPath: path } = useRouter();
 
-  if (!path) return null;
   if (path === '/_error') return null;
-  if (path === '/404') return null;
 
   const slashCount = (path.match(/\//g) || []).length;
 
   let title: string;
 
   if (slashCount === 1) {
-    const { name } = pages.find(page => page.route === path)
-    title = name === 'Home' ? 'Campos Incorporadora' : `${name} | Campos Incorporadora`;
+    const currentPage = pages.find(page => page.route === path)
+    title = currentPage.name === 'Home' ? 'Campos Incorporadora' : `${name} | Campos Incorporadora`;
   } else {
     const enterprisePath = path.split('/').pop();
 
