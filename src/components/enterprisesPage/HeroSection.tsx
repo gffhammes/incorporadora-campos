72e8d91014b155516a680e5df55d00a03d686ce8
Guilherme Hammes <gffhammes@gmail.com>
2022-05-31 11:37:15 -0300

    feat: add backgorund image to enterprises page

--- src/components/enterprisesPage/HeroSection.tsx
+++ src/components/enterprisesPage/HeroSection.tsx
@@ -4,8 +4,29 @@ import { SectionTitle } from '../commons/SectionTitle'
 
 export const HeroSection = () => {
   return (
-    <Box bgcolor='secondary.main' sx={{ height: '100%', display: 'flex' }} >
-      <Box sx={{ height: 'fit-content', width: '100%', my: 'auto' }}>
+    <Box sx={{ height: '100%', display: 'flex', position: 'relative' }} >
+      <Box
+        sx={{
+          position: 'absolute',
+          width: '100%',
+          height: '100%',
+          background: 'linear-gradient(#0E1E42, #1A50BC  50%  , rgba(1, 74, 220, .87) 80%)',
+          zIndex: 200
+        }}
+      />
+      <Box
+        sx={{
+          position: 'absolute',
+          width: '100%',
+          height: '100%',
+          backgroundImage: 'url(/images/empreendimentos-banner.png)',
+          backgroundPosition: 'bottom',
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'auto 100%',
+          zIndex: 100
+        }}
+      />
+      <Box sx={{ height: 'fit-content', width: '100%', my: 'auto', mt: '50vh', position: 'absolute', zIndex: 300 }}>
         <Container>
           <SectionTitle theme='light'>NOSSOS EMPREENDIMENTOS</SectionTitle>          
         </Container>
