327d93466cef6635ad70bc42962944701e9d3a58
Guilherme Hammes <gffhammes@gmail.com>
2022-07-05 16:06:49 -0300

    fix: add foundation to average status

--- src/helpers/getAverageEnterpriseStatus.ts
+++ src/helpers/getAverageEnterpriseStatus.ts
@@ -1,12 +1,13 @@
 interface IDetailedStatus {
   infrastructure: number;
+  foundation: number;
   masonry: number;
   installations: number;
   coating: number;
 }
 
 export const getAverageEnterpriseStatus = (detailedStatus: IDetailedStatus) => {  
-  const total = detailedStatus.infrastructure + detailedStatus.masonry + detailedStatus.installations + detailedStatus.coating;
-  const average = total / 4;
+  const total = detailedStatus.infrastructure + detailedStatus.foundation + detailedStatus.masonry + detailedStatus.installations + detailedStatus.coating;
+  const average = total / 5;
   return average;
 }
