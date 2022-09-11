681a38a8ebd57e60f20b08d2818af7fa66e50bb6
Guilherme Hammes <gffhammes@gmail.com>
2022-09-11 19:33:20 -0300

    refactor: add loop and auto play to home caroulsel and change enterprises page hero image

--- src/components/homePage/EnterprisesSlider/EnterprisesSlider.tsx
+++ src/components/homePage/EnterprisesSlider/EnterprisesSlider.tsx
@@ -1,54 +1,70 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react'
-import useEmblaCarousel from 'embla-carousel-react'
-import { Box, Skeleton, Stack, Typography } from '@mui/material'
-import Link from 'next/link'
-import Image from 'next/image'
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import { LoadingButton } from '../../commons/Button'
-import { EnterpriseCard } from './EnterpriseCard'
-import { getLocationString } from '../../../helpers/getLocationString'
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+import useEmblaCarousel from "embla-carousel-react";
+import { Box, Skeleton, Stack } from "@mui/material";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import { EnterpriseCard } from "./EnterpriseCard";
+import { getLocationString } from "../../../helpers/getLocationString";
+import Autoplay from "embla-carousel-autoplay";
 
 const sxImage = {
-  position: 'relative',
-  width: 'calc(100% + 32px)',
-  ml: '-16px',
-  aspectRatio: '1 / 1.45',
-}
+  position: "relative",
+  width: "calc(100% + 32px)",
+  ml: "-16px",
+  aspectRatio: "1 / 1.45",
+};
 
 const sxArrowWrapperCommon = {
-  position: 'absolute',
-  height: 'fit-content',
-  width: 'fit-content',
-  cursor: 'pointer',
-}
+  position: "absolute",
+  height: "fit-content",
+  width: "fit-content",
+  cursor: "pointer",
+};
 
 const sxRightArrowWrapper = {
   ...sxArrowWrapperCommon,
-  right: 'calc(1rem - 3vw)',
-}
+  right: "calc(1rem - 3vw)",
+};
 
 const sxLeftArrowWrapper = {
   ...sxArrowWrapperCommon,
-  left: 'calc(1rem - 3vw)',
-}
+  left: "calc(1rem - 3vw)",
+};
 
 interface IEnterprisesSliderProps {
   enterprises: any[];
   loading: boolean;
 }
 
-export const EnterprisesSlider = ({ enterprises, loading }: IEnterprisesSliderProps) => {
-  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false, align: 0, dragFree: true, containScroll: 'trimSnaps' });
+export const EnterprisesSlider = ({
+  enterprises,
+  loading,
+}: IEnterprisesSliderProps) => {
+  const autoplay = Autoplay({ delay: 2000 });
+
+  const [emblaRef, emblaApi] = useEmblaCarousel(
+    {
+      loop: true,
+      align: 0,
+      dragFree: true,
+    },
+    [autoplay]
+  );
   const [selectedIndex, setSelectedIndex] = useState(0);
 
-  const scrollPrev = useCallback(() => emblaApi && emblaApi.scrollPrev(), [emblaApi]);
-  const scrollNext = useCallback(() => emblaApi && emblaApi.scrollNext(), [emblaApi]);
-  const scrollTo = useCallback((index) => emblaApi && emblaApi.scrollTo(index), [emblaApi]);
+  const scrollPrev = useCallback(
+    () => emblaApi && emblaApi.scrollPrev(),
+    [emblaApi]
+  );
+  const scrollNext = useCallback(
+    () => emblaApi && emblaApi.scrollNext(),
+    [emblaApi]
+  );
+  const scrollTo = useCallback(
+    (index) => emblaApi && emblaApi.scrollTo(index),
+    [emblaApi]
+  );
 
-  const scrollPrevButtonActive = selectedIndex > 0;
-  const scrollNextButtonActive = selectedIndex < emblaApi?.scrollSnapList().length - 1;
-  
   const onSelect = useCallback(() => {
     if (!emblaApi) return;
     setSelectedIndex(emblaApi.selectedScrollSnap());
@@ -63,56 +79,87 @@ export const EnterprisesSlider = ({ enterprises, loading }: IEnterprisesSliderPr
   useEffect(() => {
     if (!enterprises || !emblaApi) return;
     emblaApi.reInit();
-  }, [emblaApi, enterprises])
-
+  }, [emblaApi, enterprises]);
 
   const slidesMemo = useMemo(() => {
     if (loading) {
-      return (
-        [0, 1, 2, 3].map(item => (
-          <Skeleton key={item} variant='rectangular' className="embla__slide" sx={{ height: '20rem', mt: 2 }}  />
-        ))
-      )
+      return [0, 1, 2, 3].map((item) => (
+        <Skeleton
+          key={item}
+          variant="rectangular"
+          className="embla__slide"
+          sx={{ height: "30rem", mt: 2 }}
+        />
+      ));
     }
 
-    return (
-      enterprises.map((enterprise, index) => (
-        <EnterpriseCard
-          key={index}
-          slug={enterprise.attributes.Slug}
-          imageUrl={enterprise.attributes.FotoHome.data.attributes.url}
-          name={enterprise.attributes.Nome}
-          status={enterprise.attributes.Status}
-          description={enterprise.attributes.Descricao1}
-          location={getLocationString(enterprise.attributes)}        
-        />
-      ))
-    )
-  }, [enterprises, loading])
+    return enterprises.map((enterprise, index) => (
+      <EnterpriseCard
+        key={index}
+        slug={enterprise.attributes.Slug}
+        imageUrl={enterprise.attributes.FotoHome.data.attributes.url}
+        name={enterprise.attributes.Nome}
+        status={enterprise.attributes.Status}
+        description={enterprise.attributes.Descricao1}
+        location={getLocationString(enterprise.attributes)}
+      />
+    ));
+  }, [enterprises, loading]);
 
   return (
-    <Stack sx={{ position: 'relative' }} spacing={4}>      
+    <Stack sx={{ position: "relative" }} spacing={4}>
       <Box className="embla" sx={{ mt: { xs: 0, md: 5 } }}>
-        <div className="embla__viewport"  ref={emblaRef}>
-          <div className="embla__container">
-            {slidesMemo}
-          </div>
+        <div className="embla__viewport" ref={emblaRef}>
+          <div className="embla__container">{slidesMemo}</div>
         </div>
-
       </Box>
-      <Stack direction='row' alignItems='center' justifyContent='center' spacing={2}>
+      <Stack
+        direction="row"
+        alignItems="center"
+        justifyContent="center"
+        spacing={2}
+      >
         {emblaApi?.scrollSnapList().map((dot, index) => (
-          <Box onClick={() => scrollTo(index)} bgcolor='secondary.main' key={index} sx={{ transition: '.2s ease all', height: '.5rem', width: '.5rem', borderRadius: '1rem', cursor: 'pointer', filter: index === selectedIndex ?  'opacity(.8)' : 'opacity(.25)' }} />
+          <Box
+            onClick={() => scrollTo(index)}
+            bgcolor="secondary.main"
+            key={index}
+            sx={{
+              transition: ".2s ease all",
+              height: ".5rem",
+              width: ".5rem",
+              borderRadius: "1rem",
+              cursor: "pointer",
+              filter: index === selectedIndex ? "opacity(.8)" : "opacity(.25)",
+            }}
+          />
         ))}
       </Stack>
-      <Stack direction='row' sx={{ position: 'absolute', height: '5rem', width: '100%', top: '50%', transform: 'translateY(-50%)' }}>
+      <Stack
+        direction="row"
+        sx={{
+          position: "absolute",
+          height: "5rem",
+          width: "100%",
+          top: "50%",
+          transform: "translateY(-50%)",
+        }}
+      >
         <Box sx={sxLeftArrowWrapper} onClick={scrollPrev}>
-          <ArrowBackIosIcon sx={{ color: scrollPrevButtonActive ? 'secondary.main' : '#c6c6c6' }} />
+          <ArrowBackIosIcon
+            sx={{
+              color: "secondary.main",
+            }}
+          />
         </Box>
         <Box sx={sxRightArrowWrapper} onClick={scrollNext}>
-          <ArrowForwardIosIcon sx={{ color: scrollNextButtonActive ? 'secondary.main' : '#c6c6c6' }} />
+          <ArrowForwardIosIcon
+            sx={{
+              color: "secondary.main",
+            }}
+          />
         </Box>
       </Stack>
     </Stack>
-  )
-}
+  );
+};
