3e46313d0700aaf19eb643d8a6c1c6e2b09f7ce5
Guilherme Hammes <gffhammes@gmail.com>
2022-09-21 18:34:46 -0300

    refactor: adjust home hero height

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,42 +1,45 @@
-import { EnterprisesSection } from '../src/components/homePage/EnterprisesSection'
-import { HistorySection } from '../src/components/homePage/HistorySection'
-import { BlogSection } from '../src/components/homePage/BlogSection'
-import { Footer } from '../src/components/commons/Footer/Footer'
-import { HeroSlider } from '../src/components/homePage/heroSlider/HeroSlider'
-import * as qs from 'qs'
+import { EnterprisesSection } from "../src/components/homePage/EnterprisesSection";
+import { HistorySection } from "../src/components/homePage/HistorySection";
+import { BlogSection } from "../src/components/homePage/BlogSection";
+import { Footer } from "../src/components/commons/Footer/Footer";
+import { HeroSlider } from "../src/components/homePage/heroSlider/HeroSlider";
+import * as qs from "qs";
+import { Box } from "@mui/material";
 
 export default function Home({ banners }) {
   return (
-    <main style={{ height: '100%' }}>
+    <Box component={"main"} sx={{ height: { xs: "60vw", md: "100%" } }}>
       <HeroSlider banners={banners} />
       <HistorySection />
       <EnterprisesSection />
       {/* <BlogSection /> */}
       <Footer />
-    </main>
-  )
+    </Box>
+  );
 }
-
 
 export async function getServerSideProps() {
   const { API_URL } = process.env;
-  
-  const bannersQuery = qs.stringify({
-    populate: [
-      'empreendimento',
-      'empreendimento.Banner',
-      'empreendimento.Logo',
-    ]
-  }, {
-    encodeValuesOnly: true, 
-  });
-  
+
+  const bannersQuery = qs.stringify(
+    {
+      populate: [
+        "empreendimento",
+        "empreendimento.Banner",
+        "empreendimento.Logo",
+      ],
+    },
+    {
+      encodeValuesOnly: true,
+    }
+  );
+
   const bannersResponse = await fetch(`${API_URL}/api/banners?${bannersQuery}`);
   const bannersData = await bannersResponse.json();
 
   return {
     props: {
       banners: bannersData.data,
-    }
-  }
+    },
+  };
 }
--- src/components/homePage/heroSlider/HeroSlider.tsx
+++ src/components/homePage/heroSlider/HeroSlider.tsx
@@ -1,99 +1,118 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react'
-import useEmblaCarousel from 'embla-carousel-react'
-import { Box, Stack } from '@mui/material'
-import { defaultSvgProps } from '../../../constants/defaultSvgProps'
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import Autoplay from 'embla-carousel-autoplay'
-import { Slide } from './Slide'
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+import useEmblaCarousel from "embla-carousel-react";
+import { Box, Stack } from "@mui/material";
+import { defaultSvgProps } from "../../../constants/defaultSvgProps";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import Autoplay from "embla-carousel-autoplay";
+import { Slide } from "./Slide";
 
 const defaultButtonProps = {
   zIndex: 500,
-  backgroundColor: 'transparent',
+  backgroundColor: "transparent",
   border: 0,
-  height: '5vw',
-  width: '5vw',
-  minHeight: '1.5rem',
-  minWidth: '1.5rem',
-  maxHeight: '3rem',
-  maxWidth: '3rem',
-  borderRadius: '20rem',
-  cursor: 'pointer',
-  position: 'absolute',
-}
+  height: "5vw",
+  width: "5vw",
+  minHeight: "1.5rem",
+  minWidth: "1.5rem",
+  maxHeight: "3rem",
+  maxWidth: "3rem",
+  borderRadius: "20rem",
+  cursor: "pointer",
+  position: "absolute",
+};
 
 const sxEmbla = {
-  overflowX: 'hidden',
-  overflowY: 'visible',
-  height: '100%'
-}
+  overflowX: "hidden",
+  overflowY: "visible",
+  height: "100%",
+};
 
 const sxEmblaContainer = {
-  display: 'flex',
-  height: '100%'
-}
+  display: "flex",
+  height: "100%",
+};
 
- const sxEmblaSlide = {
-  position: 'relative',
-  flex: '0 0 100%',
-  marginRight: 0,  
-  transitionProperty: 'filter, box-shadow',
-  transitionDuration: '1s',
-  transitionTimingFunction: 'ease',
-  width: '100%',
-  aspectRatio: '2 / 1',
-}
+const sxEmblaSlide = {
+  position: "relative",
+  flex: "0 0 100%",
+  marginRight: 0,
+  transitionProperty: "filter, box-shadow",
+  transitionDuration: "1s",
+  transitionTimingFunction: "ease",
+  width: "100%",
+  aspectRatio: "2 / 1",
+};
 
 export const HeroSlider = ({ banners }) => {
-  const autoplay = Autoplay({ delay: 5000 })
-  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [autoplay])
-  const [selectedSlide, setSelectedSlide] = useState(0)
-  const [dots, setDots] = useState([])
+  const autoplay = Autoplay({ delay: 5000 });
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [autoplay]);
+  const [selectedSlide, setSelectedSlide] = useState(0);
+  const [dots, setDots] = useState([]);
 
   useEffect(() => {
     const arr = [];
     for (let i = 0; i < banners.length; i++) {
-      arr.push({ active: false })
+      arr.push({ active: false });
     }
     arr[0].active = true;
     setDots(arr);
-  }, [banners])
+  }, [banners]);
 
   useEffect(() => {
-    setDots((dots): any[] => dots.map((dot, index) => index === selectedSlide ? { active: true } : { active: false } ))
-  }, [selectedSlide])
+    setDots((dots): any[] =>
+      dots.map((dot, index) =>
+        index === selectedSlide ? { active: true } : { active: false }
+      )
+    );
+  }, [selectedSlide]);
 
   const changeSelectedSlide = (newSlideIndex) => {
-    emblaApi.scrollTo(newSlideIndex)
-  }
+    emblaApi.scrollTo(newSlideIndex);
+  };
 
   const scrollPrev = useCallback(() => {
-    if (emblaApi) emblaApi.scrollPrev()
-  }, [emblaApi])
+    if (emblaApi) emblaApi.scrollPrev();
+  }, [emblaApi]);
 
   const scrollNext = useCallback(() => {
-    if (emblaApi) emblaApi.scrollNext()
-  }, [emblaApi])
+    if (emblaApi) emblaApi.scrollNext();
+  }, [emblaApi]);
 
-  emblaApi?.on('select', () => {
-    setSelectedSlide(emblaApi.selectedScrollSnap())
-  })
+  emblaApi?.on("select", () => {
+    setSelectedSlide(emblaApi.selectedScrollSnap());
+  });
 
   return (
-    <Stack direction='row' alignItems='center' sx={{ height: '100%', width: '100%' }}>
-      <Box sx={{ ...defaultButtonProps, marginLeft: '10vw', left: 0 }} onClick={scrollPrev}>        
-        <ArrowBackIosIcon {...defaultSvgProps} sx={{ color: 'white' }} />
+    <Stack
+      direction="row"
+      alignItems="center"
+      sx={{ height: "100%", width: "100%" }}
+    >
+      <Box
+        sx={{ ...defaultButtonProps, marginLeft: "10vw", left: 0 }}
+        onClick={scrollPrev}
+      >
+        <ArrowBackIosIcon {...defaultSvgProps} sx={{ color: "white" }} />
       </Box>
-      <Box sx={{ width: '100%', height: '100%'}}>
+      <Box sx={{ width: "100%", height: "100%" }}>
         <Box sx={sxEmbla}>
-          <Box sx={{ height: '100%' }} ref={emblaRef}>
+          <Box sx={{ height: "100%" }} ref={emblaRef}>
             <Box sx={sxEmblaContainer}>
               {banners.map((slide, index) => (
-                <Box sx={sxEmblaSlide} key={index} >
+                <Box sx={sxEmblaSlide} key={index}>
                   <Slide
-                    bgImage={slide.attributes.empreendimento.data.attributes.Banner.data.attributes.url}
-                    logo={slide.attributes.empreendimento.data.attributes.Logo.data.attributes.url}
-                    enterpriseName={slide.attributes.empreendimento.data.attributes.Nome}
+                    bgImage={
+                      slide.attributes.empreendimento.data.attributes.Banner
+                        .data.attributes.url
+                    }
+                    logo={
+                      slide.attributes.empreendimento.data.attributes.Logo.data
+                        .attributes.url
+                    }
+                    enterpriseName={
+                      slide.attributes.empreendimento.data.attributes.Nome
+                    }
                     text={slide.attributes.Texto}
                     buttonText={slide.attributes.TextoBotao}
                     href={slide.attributes.empreendimento.data.attributes.Slug}
@@ -104,14 +123,38 @@ export const HeroSlider = ({ banners }) => {
           </Box>
         </Box>
       </Box>
-      <Box sx={{ ...defaultButtonProps, marginRight: '10vw', right: 0 }} onClick={scrollNext}>        
-        <ArrowForwardIosIcon {...defaultSvgProps} sx={{ color: 'white' }} />
+      <Box
+        sx={{ ...defaultButtonProps, marginRight: "10vw", right: 0 }}
+        onClick={scrollNext}
+      >
+        <ArrowForwardIosIcon {...defaultSvgProps} sx={{ color: "white" }} />
       </Box>
-      <Box sx={{ position: 'absolute', display: 'flex', bottom: '0', left: '50%', transform: 'translate(-50%, -2rem)', gap: '1rem' }}>
+      <Box
+        sx={{
+          position: "absolute",
+          display: "flex",
+          bottom: "0",
+          left: "50%",
+          transform: "translate(-50%, -2rem)",
+          gap: "1rem",
+        }}
+      >
         {dots.map((dot, index) => (
-          <Box onClick={() => changeSelectedSlide(index)} bgcolor='white' key={index} sx={{ transition: '.2s ease all', height: '.5rem', width: '.5rem', borderRadius: '1rem', cursor: 'pointer', filter: dot.active ?  'opacity(.8)' : 'opacity(.25)' }} />
+          <Box
+            onClick={() => changeSelectedSlide(index)}
+            bgcolor="white"
+            key={index}
+            sx={{
+              transition: ".2s ease all",
+              height: ".5rem",
+              width: ".5rem",
+              borderRadius: "1rem",
+              cursor: "pointer",
+              filter: dot.active ? "opacity(.8)" : "opacity(.25)",
+            }}
+          />
         ))}
       </Box>
     </Stack>
-  )
-}
+  );
+};
