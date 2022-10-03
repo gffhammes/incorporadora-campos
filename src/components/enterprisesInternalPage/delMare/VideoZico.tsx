3e7ff1fab20bf7d58088824535caa10db2c73d95
Guilherme Hammes <gffhammes@gmail.com>
2022-10-03 19:48:56 -0300

    refactor: change video zico and make it responsive

--- src/components/enterprisesInternalPage/delMare/VideoZico.tsx
+++ src/components/enterprisesInternalPage/delMare/VideoZico.tsx
@@ -1,22 +1,45 @@
-import { Box, Container } from '@mui/material'
-import React from 'react'
+import { Box, Container } from "@mui/material";
+import React from "react";
+import { useBreakpoint } from "../../../hooks/useBreakpoint";
 
-type Props = {}
+type Props = {};
 
 const VideoZico = (props: Props) => {
+  const { md } = useBreakpoint();
   return (
     <Box>
-      <Container sx={{ pb: 5, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-        <video
-          src='/videos/VT CAMPOS INCORPORADORA ALT3.mp4'
-          width='60%'
-          autoPlay
-          controls
-          muted
-        />
+      <Container
+        sx={{
+          pb: 5,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Box
+          sx={{
+            position: "relative",
+            width: { xs: "100%", md: "60%" },
+            pb: { xs: "56.25%", md: "33.75%" },
+          }}
+        >
+          <iframe
+            src="https://drive.google.com/file/d/1s_3CCJpqLRcPIpLUS0vE58r7wshXGCxE/preview"
+            width="100%"
+            height="100%"
+            allow="autoplay"
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+            }}
+          />
+        </Box>
       </Container>
     </Box>
-  )
-}
+  );
+};
 
-export default VideoZico
+export default VideoZico;
