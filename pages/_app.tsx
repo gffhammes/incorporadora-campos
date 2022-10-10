f5f3e40b1d576fe4ba268bedd5a94672b1380e11
Guilherme Hammes <gffhammes@gmail.com>
2022-10-10 10:37:56 -0300

    refactor: change GTM tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,47 +1,46 @@
-import { Header } from '../src/components/commons/Header/Header'
-import '../styles/globals.css'
-import { Box, createTheme, ThemeProvider } from '@mui/material'
-import { WhatsAppFloatButton } from '../src/components/commons/WhatsAppFloatButton';
-import { PageTitle } from '../src/components/commons/PageTitle';
-import TagManager from 'react-gtm-module'
-import { useEffect } from 'react';
-import Router, { useRouter } from "next/router"
-import NProgress from "nprogress"
-import Head from 'next/head';
+import { Header } from "../src/components/commons/Header/Header";
+import "../styles/globals.css";
+import { createTheme, ThemeProvider } from "@mui/material";
+import { WhatsAppFloatButton } from "../src/components/commons/WhatsAppFloatButton";
+import { PageTitle } from "../src/components/commons/PageTitle";
+import TagManager from "react-gtm-module";
+import { useEffect } from "react";
+import Router, { useRouter } from "next/router";
+import NProgress from "nprogress";
 
 const theme = createTheme({
   typography: {
-    fontFamily: 'Satoshi, sans-serif',
+    fontFamily: "Satoshi, sans-serif",
   },
   palette: {
-    primary: { main: '#0E1E42' },
-    secondary: { main: '#1A47BC' },
-  }
-})
+    primary: { main: "#0E1E42" },
+    secondary: { main: "#1A47BC" },
+  },
+});
 
-Router.events.on('routeChangeStart', url => {
-  NProgress.start()
-})
+Router.events.on("routeChangeStart", (url) => {
+  NProgress.start();
+});
 
-Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on("routeChangeComplete", () => NProgress.done());
 
-Router.events.on('routeChangeError', () => NProgress.done()) 
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter()
+  const router = useRouter();
 
   useEffect(() => {
-    TagManager.initialize({ gtmId: 'GTM-NZV49Z7' });
+    TagManager.initialize({ gtmId: "GTM-W7TDPT6" });
   }, []);
 
-  return (    
+  return (
     <ThemeProvider theme={theme}>
       <PageTitle />
       <Header />
       <Component {...pageProps} />
       <WhatsAppFloatButton />
     </ThemeProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
