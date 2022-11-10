c7e6e0f35bb13f1af88b29478c43926b6176e073
Guilherme Hammes <gffhammes@gmail.com>
2022-11-10 19:16:02 -0300

    feat: add blog page

---
+++ pages/blog/index.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import { HeroSection } from "../../src/components/blog/HeroSection";
+
+const Blog = () => {
+  return (
+    <Box component="main" sx={{ height: "100%" }}>
+      <HeroSection />
+    </Box>
+  );
+};
+
+export default Blog;
--- src/constants/pages.ts
+++ src/constants/pages.ts
@@ -1,26 +1,26 @@
 export const pages = [
   {
-    route: '/',
-    name: 'Home'
+    route: "/",
+    name: "Home",
   },
   {
-    route: '/sobre-nos',
-    name: 'Sobre Nós'
+    route: "/sobre-nos",
+    name: "Sobre Nós",
   },
   {
-    route: '/empreendimentos',
-    name: 'Empreendimentos'
-  },
-  // {
-  //   route: '/blog',
-  //   name: 'Blog'
-  // },
-  {
-    route: '/central-de-vendas',
-    name: 'Central de Vendas'
+    route: "/empreendimentos",
+    name: "Empreendimentos",
   },
   {
-    route: '/contato',
-    name: 'Contato'
+    route: "/blog",
+    name: "Blog",
   },
-]
+  {
+    route: "/central-de-vendas",
+    name: "Central de Vendas",
+  },
+  {
+    route: "/contato",
+    name: "Contato",
+  },
+];
