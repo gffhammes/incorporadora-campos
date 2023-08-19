af31171b3a0710b8ef087d204503ccedb46fbebe
Guilherme Hammes <gffhammes@gmail.com>
2023-08-19 13:25:37 -0300

    feat: adjust responsivity

--- src/components/aboutPage/DescriptionSection/DescriptionSection.tsx
+++ src/components/aboutPage/DescriptionSection/DescriptionSection.tsx
@@ -1,15 +1,5 @@
 import { Box, Container, Stack, Typography } from "@mui/material";
 import React from "react";
-import { useBreakpoint } from "../../../hooks/useBreakpoint";
-import { ImageWithHover } from "./ImageWithHover";
-import { Text } from "./Text";
-
-const text = `<strong>A Campos Incorporadora tem em seu DNA a inspiração.</strong>
-
-Nasceu inspirada na conexão entre amigos motivados pelo desejo de crescer, buscar algo novo e empreender. O que era desejo se tornou realidade através da formação de um grupo de investimentos. Grupo este que olha para o futuro, que busca o resultado através da relação "ganha-ganha" e que preza pela qualidade de vida dos seus parceiros.
-
-Seus empreendimentos foram cuidadosamente nominados com praças italianas. Que denota robustez, imponência, características arquitetônicas marcantes, planejamento dos espaços, convergência e centralidade.
-`;
 
 const text1 = `A Campos Incorporadora tem em seu DNA a inspiração.
 
@@ -22,25 +12,6 @@ Grupo este que olha para o futuro, que busca o resultado através da relação "
 Seus empreendimentos foram cuidadosamente nominados com praças italianas, que denota robustez, imponência, características arquitetônicas marcantes, planejamento dos espaços, convergência e centralidade.`;
 
 const DescriptionSection = () => {
-  const { sm, md, lg } = useBreakpoint();
-
-  const getStackDirection = () => {
-    if (lg) return "row";
-
-    return "column";
-  };
-
-  const getStackSpacing = () => {
-    if (md) return 10;
-    if (sm) return 7;
-
-    return 5;
-  };
-
-  const stackDirection = getStackDirection();
-
-  const stackSpacing = getStackSpacing();
-
   return (
     <Box id="description">
       <Container
@@ -55,7 +26,7 @@ const DescriptionSection = () => {
           <Stack spacing={4}>
             <Typography
               fontFamily="couturier"
-              fontSize={64}
+              fontSize={{ xs: 42, sm: 56, md: 64 }}
               lineHeight={1}
               letterSpacing={-2}
               sx={{ fontVariantLigatures: "none" }}
