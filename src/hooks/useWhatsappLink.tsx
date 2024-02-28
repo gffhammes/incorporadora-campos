92b8091be35f16274fe8d710831b2dee55740e02
Guilherme Hammes <gffhammes@gmail.com>
2024-02-28 18:20:53 -0300

    feat: adjust whatsapp link based on route

--- src/hooks/useWhatsappLink.tsx
+++ src/hooks/useWhatsappLink.tsx
@@ -1,37 +1,35 @@
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { useMemo } from "react";
 import { useGlobalsContext } from "../contexts/useGlobalsContext";
+import { enterprises } from "../data/enterprises";
 
 export const useWhatsappLink = () => {
   const { data: globalData } = useGlobalsContext();
   const { asPath: currentPath } = useRouter();
-  const { API_URL } = process.env;
-  const [data, setData] = useState<any>(null);
   let text: string;
 
   const whatsappNumber = `55${globalData.phone
     .replaceAll(" ", "")
     .replaceAll("-", "")}`;
 
-  useEffect(() => {
+  const enterpriseName = useMemo(() => {
     const slugArray = currentPath.split("/");
 
     if (slugArray.length !== 3) {
-      setData(null);
       return;
     }
 
-    const enterpriseName = slugArray.pop();
+    const enterpriseSlug = slugArray.pop();
 
-    fetch(`${API_URL}/api/empreendimentos?filters[Slug][$eq]=${enterpriseName}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data.data[0]);
-      });
-  }, [API_URL, currentPath]);
+    const enterpriseName = enterprises.find(
+      (enterprise) => enterprise.attributes.Slug === enterpriseSlug
+    )?.attributes.Nome;
 
-  if (data) {
-    text = `Olá! Vim pelo site e gostaria de mais informações sobre o empreendimento ${data.attributes.Nome}!`;
+    return enterpriseName;
+  }, [currentPath]);
+
+  if (enterpriseName) {
+    text = `Olá! Vim pelo site e gostaria de mais informações sobre o empreendimento ${enterpriseName}!`;
   } else {
     text = "Olá! Vim pelo site e gostaria de mais informações!";
   }
